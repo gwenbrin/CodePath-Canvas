1667320daab723021c0985982659f700ce6fa44d
Gwen Brinsmead <gwen@Gwens-MacBook-Pro.local>
2014-07-01 20:21:49 -0700

    Add gif image

---
+++ Canvas/CanvasViewController.m
@@ -0,0 +1,38 @@
+//
+//  CanvasViewController.m
+//  Canvas
+//
+//  Created by Gwen Brinsmead on 6/29/14.
+//  Copyright (c) 2014 Gwen Brinsmead. All rights reserved.
+//
+
+#import "CanvasViewController.h"
+
+@interface CanvasViewController ()
+
+@end
+
+@implementation CanvasViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
